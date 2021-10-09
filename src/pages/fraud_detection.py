71dfe94b5fd8799a081ebac1f15f92345082b4c6
MiguelLaraC <miguelandres.lara@gmail.com>
2021-10-08 21:41:52 -0500

    changed the menu for navigation

--- src/pages/fraud_detection.py
+++ src/pages/fraud_detection.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
 def write():
-	if st.session_state['navigation_current'] != "Fraud Detection":
-		st.session_state['navigation_current'] = "Fraud Detection"
-		st.session_state['navigation_changed'] = True
-	else:
-		st.session_state['navigation_changed'] = False
+    if st.session_state['navigation_current'] == None:
+        st.session_state['navigation_current'] = "Fraud Detection"
+        st.session_state['navigation_changed'] = False
+    elif (st.session_state['navigation_current'] != "Fraud Detection"):
+        st.session_state['navigation_current'] = "Fraud Detection"
+        st.session_state['navigation_changed'] = True
+    elif (st.session_state['navigation_current'] == "Fraud Detection"):
+        st.session_state['navigation_changed'] = False
 
 
 st.title("Fraud Detection")
