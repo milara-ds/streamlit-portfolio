feafc4d2ef12485dddba825e62a94b9e58ba1b16
MiguelLaraC <miguelandres.lara@gmail.com>
2021-10-08 21:50:04 -0500

    identation corrected

--- src/pages/fraud_detection.py
+++ src/pages/fraud_detection.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 def write():
     if st.session_state['navigation_current'] == None:
         st.session_state['navigation_current'] = "Fraud Detection"
@@ -9,9 +10,10 @@ def write():
         st.session_state['navigation_changed'] = True
     elif (st.session_state['navigation_current'] == "Fraud Detection"):
         st.session_state['navigation_changed'] = False
+        
+    st.title("Fraud Detection")
+    st.write("---")
 
+    st.write("Working on the page...")
 
-	st.title("Fraud Detection")
-	st.write("---")
-
-	st.write("Working on the page...")
+    st.write("In the meantime, please visit the complete analysis [here](https://github.com/milara-ds/targeted-marketing/blob/main/Uplift_v1.ipynb).")
